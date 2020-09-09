fe63a0d22e0e3dd9814364962b3599f1bd0022f3
KeanuLox <skyloxkeanu@gmail.com>
2020-09-09 14:43:34 +0200

    init

---
+++ packages/wantedLevel/index.js
@@ -0,0 +1,28 @@
+var globalLevel = 0;
+mp.events.add("globalWantedLevel",(player,level) => {
+  globalLevel = level;
+  mp.players.forEach(_player => {
+    _player.call("wantedLevel",[globalLevel]);
+  });
+});
+mp.events.add("wantedLevel",(player,level) => {
+  player.call("wantedLevel",[level]);
+});
+
+mp.events.add("playerJoin",(player) => {
+  player.call("wantedLevel",[globalLevel]);
+});
+
+
+
+
+mp.events.addCommand("globalWanted",(player,wantedLevel) => {
+  globalLevel = Number(wantedLevel);
+  mp.players.forEach(_player => {
+    _player.call("wantedLevel",[globalLevel]);
+  });
+});
+
+mp.events.addCommand("wanted",(player,wantedLevel) => {
+  player.call("wantedLevel",[Number(wantedLevel)]);
+});
